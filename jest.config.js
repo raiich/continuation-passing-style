c911d0467bf531e46f19c94c2538a985029f2d1f
Ichiro Arai <upgrade.outdated@gmail.com>
2019-12-11 00:43:17 +0900

    add basic files

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "preset": "ts-jest",
+    "moduleFileExtensions": [
+        "ts",
+        "js"
+    ],
+    "transform": {
+        "^.+\\.ts$": "ts-jest"
+    },
+    "globals": {
+        "ts-jest": {
+            "tsConfig": "tsconfig.json"
+        }
+    },
+    "testMatch": [
+        "**/tests/**/*.test.ts"
+    ],
+    "moduleNameMapper": {
+        "^@/(.+)": "<rootDir>/src/$1"
+    }
+}
